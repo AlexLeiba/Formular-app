2c4012dfd15e64549c3a102c39fd9ccd08efc38c
AlexLeiba <Alexleiba13@gmail.com>
2022-01-03 15:12:20 +0200

    changes

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ const form = document.querySelector("form");
 const datele = document.querySelector(".datele");
 
 const deleteB = document.querySelector(".deleteB");
-
 const errorParagraph = document.querySelector(".error");
 
 nameUser.addEventListener("input", function (event) {
   const userValue = event.target.value;
   if (!isNaN(userValue) && userValue.length > 0) {
+    errorParagraph.style.visibility = "visible";
     errorParagraph.innerText = "Ati introdus valori numerice!";
   } else if (isNaN(userValue) || userValue.length === 0) {
+    errorParagraph.style.visibility = "hidden";
     errorParagraph.innerText = "";
   }
 });
@@ -22,8 +23,10 @@ nameUser.addEventListener("input", function (event) {
 surname.addEventListener("input", function (event) {
   const userValue = event.target.value;
   if (!isNaN(userValue) && userValue.length > 0) {
+    errorParagraph.style.visibility = "visible";
     errorParagraph.innerText = "Ati introdus valori numerice!";
   } else if (isNaN(userValue) || userValue.length === 0) {
+    errorParagraph.style.visibility = "hidden";
     errorParagraph.innerText = "";
   }
 });
@@ -31,8 +34,10 @@ surname.addEventListener("input", function (event) {
 email.addEventListener("change", function (event) {
   const userValue = event.target.value;
   if (userValue.length < 5 && userValue.length > 0) {
+    errorParagraph.style.visibility = "visible";
     errorParagraph.innerText = "Ati introdus prea putine caractere!";
   } else if (userValue.length > 4 || userValue.length === 0) {
+    errorParagraph.style.visibility = "hidden";
     errorParagraph.innerText = "";
   }
 });
@@ -41,9 +46,11 @@ number.addEventListener("change", function (event) {
   const userValue = event.target.value;
 
   if (userValue.length <= 7) {
+    errorParagraph.style.visibility = "visible";
     errorParagraph.innerText = "Ati introdus prea putine caractere numerice!";
   } else if (userValue.length === 0 || userValue.length >= 7) {
     errorParagraph.innerText = "";
+    errorParagraph.style.visibility = "hidden";
   }
 });
 
@@ -65,9 +72,11 @@ form.addEventListener("submit", function (event) {
   const numberValid = valueNumber.length > 7;
 
   if (nameValid && surnameValid && emailValid && numberValid) {
-    datele.innerText = `Datele introduse sunt:                                   Numele: ${valueName}, Prenumele: ${valueSurname}, Email: ${valueEmail}, Telephone Number: ${valueNumber}`;
+    datele.innerText = `Datele introduse sunt:  Numele: ${valueName}, Prenumele: ${valueSurname}, Email: ${valueEmail}, Telephone Number: ${valueNumber}`;
   } else {
-    errorParagraph.innerText = "Aveti eroari in formular!";
+    errorParagraph.style.visibility = "visible";
+    errorParagraph.innerText =
+      "Se pare ca aveti eroari sau nu ati introdus nimic!";
     datele.innerText = "";
   }
 });
@@ -75,6 +84,14 @@ form.addEventListener("submit", function (event) {
 // Button delete
 
 deleteB.addEventListener("click", function () {
+  const property = getComputedStyle(deleteB);
+  let valueP = property.getPropertyValue("color");
+  if (valueP === "rgb(0, 0, 0)") {
+    deleteB.style.color = "rgb(255, 255, 255)";
+  } else if (valueP === "rgb(255, 255, 255)") {
+    deleteB.style.color = "rgb(0, 0, 0)";
+  }
+
   datele.innerText = "";
   nameUser.value = "";
   surname.value = "";
@@ -82,4 +99,5 @@ deleteB.addEventListener("click", function () {
   number.value = "";
 
   errorParagraph.innerText = "";
+  errorParagraph.style.visibility = "hidden";
 });
